21d257b2da5c23ff4e03f94cfa0d8104cbd87381
kowasa <kowasa88@gmail.com>
2020-09-06 11:56:32 +0000

    modified index

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -28,7 +28,7 @@ class TasksController < ApplicationController
     end
     
     def update
-        @task = Task.find(params[:if])
+        @task = Task.find(params[:id])
         
         if @task.update(task_params)
             flash[:success] = "Task は正常に更新されました"
@@ -48,7 +48,7 @@ class TasksController < ApplicationController
     end
     
     def task_params
-        params.require(:task).permit(:content, :status)
+        params.require(:task).permit(:content)
     end
     
 end
